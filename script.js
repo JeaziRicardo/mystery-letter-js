79dfba32a8e4eb3b48ca85614d279b8f0598521d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-21 19:31:34 -0300

    Adiciona uma mensagem se o conteudo da carta estiver em branco

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const button = document.querySelector('#criar-carta');
 const paragraph = document.querySelector('#carta-gerada');
 
 function creatMisteryLetter() {
-  let text = input.value;
-  let array = text.split(' ');
+  const text = input.value;
+  const array = text.split(' ');
   paragraph.innerHTML = '';
-  for (let index = 0; index < array.length; index += 1) {
-    const span = document.createElement('span');
-    span.innerHTML = array[index];
-    paragraph.appendChild(span);
+  if (!text.replace(/\s/g, '').length) { //https://stackoverflow.com/questions/10261986/how-to-detect-string-which-contains-only-spaces - A função usa uma espressão regular para verificar se a string tem apenas espaços vazios.
+    paragraph.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    for (let index = 0; index < array.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerHTML = array[index];
+      paragraph.appendChild(span);
+    }
   }
 }
 button.addEventListener('click', creatMisteryLetter);
